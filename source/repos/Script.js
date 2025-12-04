f95c6938ae656b36546ce98e484d93b44499fb4a
Jason Salas <upserge@gmail.com>
2025-12-04 09:07:00 -0800

    Script.js, numGuesserStyles.css, and hello2

--- source/repos/Script.js
+++ source/repos/Script.js
@@ -1,2 +1,32 @@
-// JavaScript source code
-console.log("Hello World!");
+// Declare some global variables
+var userGuess = document.getElementById("guessedNumber");
+const submitButton = document.getElementById('submitButton');
+
+// Function to handle the guessing game logic
+function guessingGame() { 
+    submitButton.addEventListener('click', () => {
+        var magicNumber = Math.floor(Math.random() * 10) + 1;
+        const guess = userGuess.value;
+
+        if (parseInt(guess) === parseInt(magicNumber)) {
+            let result = "Congratulations! You guessed the magic number! You're cracked out of your mind.";
+            document.getElementById("resultText").innerHTML = result;
+        }
+        else {
+            let result = "Another wrong guess unfortunately. The magic number was " + magicNumber + ". Better luck next time!";
+            document.getElementById("resultText").innerHTML = result;
+    }
+
+        console.log("1 loop completed"); 
+    });
+}
+
+// If players want to play again, clear the guessing box and call the function again
+const playAgainButton = document.getElementById('playAgain');
+playAgainButton.addEventListener('click', () => {
+    document.getElementById("guessedNumber").value = "";
+    guessingGame();
+});
+
+// Initialize the game
+guessingGame();
